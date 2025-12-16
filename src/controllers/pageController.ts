c223d7681add4a75b61a7f2b1d6a1872ef0f49ac
Lino Menezes <Linogomes54@hotmail.com>
2025-12-16 14:28:41 -0300

    feature: Novas Animais nas Abas

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,41 +1,50 @@
 import {Request, Response} from 'express'
 
+import {Pet} from '../models/pet'
 import {createMenuObject} from '../helpers/createMenuObject' 
 
-export const home = (req: Request, res: Response) => {
 
+export const home = (req: Request, res: Response) => {
+    let list = Pet.getAll()
     res.render('pages/page',{
         menu:createMenuObject('all'),
         banner:{
             tittle:'Todos os Animais',
             background:'allanimals.jpg'
-        } 
+        },
+        list 
     }) 
 }
 export const dogs = (req: Request, res: Response) => {
+    let list = Pet.getFromType('dog')
     res.render('pages/page',{
         menu:createMenuObject('dog'),
         banner:{
             tittle:"Cachorros",
             background:"banner_dog.jpg"
-        }
+        },
+        list
     })
 }
 export const cats = (req: Request, res: Response) => {
+    let list = Pet.getFromType('cat')
     res.render('pages/page',{
         menu:createMenuObject('cat'),
         banner:{
             tittle:"Gatos",
             background:"banner_cat.jpg"
-        }
+        },
+        list
     })
 }
 export const fishes = (req: Request, res: Response) => {
+    let list = Pet.getFromType('fish')
     res.render('pages/page',{
         menu:createMenuObject('fish'),
         banner:{
             tittle:"Peixes",
             background:"banner_fish.jpg"
-        }
+        },
+        list
     })
 }
